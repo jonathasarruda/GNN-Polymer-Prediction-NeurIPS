85a5427a8e64e08691f28d35712cde391bb5a49a
Jonathas dos Santos Arruda <39897924+jonathasarruda@users.noreply.github.com>
2025-11-20 15:13:24 -0400

    model_service.py

--- model_service.py
+++ model_service.py
@@ -3,9 +3,9 @@ from gnn_polymer_predictor import SimpleGNN
 
 class ModelService:
     def __init__(self):
-        in_channels = 128          # ou x_all.shape[1] se quiser calcular dinamicamente
-        hidden_channels = 64       # confirmado no seu código
-        out_channels = 5           # confirmado via target_cols
+        in_channels = 128          
+        hidden_channels = 64       
+        out_channels = 5          
 
         self.model = SimpleGNN(in_channels, hidden_channels, out_channels)
         self.model.load_state_dict(torch.load("model/simple_gnn.pt", map_location="cpu"))
